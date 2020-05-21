21328bdabff627c083d3128b492e27f9910bd25e
João Nelas <joao.nelas@gmail.com>
2020-05-21 12:28:23 +0100

    integrações cob: auth e comunicação por postMessage

--- src/plugins/dashboard/public/application/dashboard_app_controller.tsx
+++ src/plugins/dashboard/public/application/dashboard_app_controller.tsx
@@ -364,6 +364,17 @@ export class DashboardAppController {
         refreshInterval: timefilter.getRefreshInterval(),
       };
       $scope.panels = dashboardStateManager.getPanels();
+
+      // mark hipotese 1
+      // const bounds = timefilter.getBounds();
+      // const msg = {
+      //   fromKibana: true,
+      //   query: $scope.model.query,
+      //   filters: $scope.model.filters.map(f => f.query),
+      //   time: { min: bounds.min.valueOf(), max: bounds.max.valueOf() }
+      // };
+      // window.console.debug('COB', msg);
+      // if (window.parent) window.parent.postMessage(msg, '*');
     };
 
     updateState();
@@ -433,6 +444,17 @@ export class DashboardAppController {
                 );
                 dirty = true;
               }
+
+              // mark hipotese 2
+              const bounds = timefilter.getBounds();
+              const msg = {
+                fromKibana: true,
+                query: $scope.model.query,
+                filters: container.getInput().filters.map(f => f.query),
+                time: { min: bounds.min.valueOf(), max: bounds.max.valueOf() },
+              };
+              window.console.debug('COB', msg);
+              if (window.parent) window.parent.postMessage(msg, '*');
 
               dashboardStateManager.handleDashboardContainerChanges(container);
               $scope.$evalAsync(() => {
@@ -1127,9 +1149,20 @@ export class DashboardAppController {
       updateNavBar();
     });
 
+    function receiveExternalQuery(event) {
+      if (!event.data || !event.data.query || event.data.fromKibana) return;
+      window.console.debug('COB', 'message', event.data, event);
+      // as duas hipóteses parecem funcionar, ficam como referência
+      // dashboardStateManager.setQuery(event.data.query);
+      $scope.updateQueryAndFetch(event.data);
+    }
+    window.addEventListener('message', receiveExternalQuery, false);
+
     $scope.$on('$destroy', () => {
       // we have to unmount nav bar manually to make sure all internal subscriptions are unsubscribed
       unmountNavBar();
+
+      window.removeEventListener('message', receiveExternalQuery);
 
       updateSubscription.unsubscribe();
       stopSyncingQueryServiceStateWithUrl();
